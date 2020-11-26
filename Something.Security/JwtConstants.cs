ca01e2cc364d587a0437b61b6f4e94834bc372b0
Stephen Green <SGreen@qicglobal.com>
2020-11-26 15:46:29 +0000

    tidying

--- Something.Security/JwtConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Something.Security
-{
-    public class JwtConstants
-    {
-        public const string Audience = @"https://localhost:44310/";
-        public const string Issuer = @"https://localhost:44310/";
-        public const string Secret = "o8y7o4lno8y7o4lno8y7o4lno8y7o4lno8y7o4lno8y7o4lno8y7o4ln";
-    }
-}
